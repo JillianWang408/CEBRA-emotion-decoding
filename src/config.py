b0de48f4bf457ea7cdf0da39e17da487e7e2ba22
Jillian Wang <jillian_wang@berkeley.edu>
2025-07-22 10:10:52 -0700

    new image for patient_1

--- src/config.py
+++ src/config.py
@@ -6,15 +6,32 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.append(str(PROJECT_ROOT))
 
-# DATA_ROOT = Path("/Users/wangzihan/Desktop/Projects/xCEBRA/data/EC272/Patient1")
-# MODEL_DIR = PROJECT_ROOT /"output" / "patient_1"
-# DATA_DIR = PROJECT_ROOT / "data" / "EC272" / "Patient1"
+PATIENT_ID = 1
 
-DATA_ROOT = Path("/Users/wangzihan/Desktop/Projects/xCEBRA/data/EC301/Patient2")
-MODEL_DIR = PROJECT_ROOT / "output" / "patient_2" / "models"
-EVALUATION_OUTPUT_DIR = PROJECT_ROOT / "output" / "patient_2" / "evaluation_outputs"
-ATTRIBUTION_OUTPUT_DIR = PROJECT_ROOT / "output" / "patient_2" / "attribution_outputs"
-DATA_DIR = PROJECT_ROOT / "data" / "EC301" / "Patient2"
+if PATIENT_ID == 1:
+    DATA_ROOT = Path("/Users/wangzihan/Desktop/Projects/xCEBRA/data/EC272/Patient1")
+    MODEL_DIR = PROJECT_ROOT /"output" / "patient_1" / "models"
+    EVALUATION_OUTPUT_DIR = PROJECT_ROOT / "output" / "patient_1" / "evaluation_outputs"
+    ATTRIBUTION_OUTPUT_DIR = PROJECT_ROOT / "output" / "patient_1" / "attribution_outputs"
+    DATA_DIR = PROJECT_ROOT / "data" / "EC272" / "Patient1"
+    ELECTRODE_NAMES = [
+    'LOFC7', 'LOFC8', 'LOFC9', 'LOFC10', 'LOFC2', 'LOFC3', 'LOFC4', 'LOFC5',
+    'ROFC2', 'ROFC3', 'ROFC4', 'ROFC5', 'ROFC7', 'ROFC8', 'ROFC9', 'ROFC10',
+    'LAMY1', 'LAMY2', 'LAMY3', 'LAMY4', 'LAINS1', 'LAINS2', 'LAINS3', 'LAINS4',
+    'LCING2', 'LCING3', 'LCING4', 'LCING5','RCING2', 'RCING3', 'RCING4', 'RCING5',
+    'RAINS1', 'RAINS2', 'RAINS3', 'RAINS4','RAMY1', 'RAMY2', 'RAMY3', 'RAMY4'
+]   
+if PATIENT_ID == 2:
+    DATA_ROOT = Path("/Users/wangzihan/Desktop/Projects/xCEBRA/data/EC301/Patient2")
+    MODEL_DIR = PROJECT_ROOT /"output" / "patient_2" / "models"
+    EVALUATION_OUTPUT_DIR = PROJECT_ROOT / "output" / "patient_2" / "evaluation_outputs"
+    ATTRIBUTION_OUTPUT_DIR = PROJECT_ROOT / "output" / "patient_2" / "attribution_outputs"
+    DATA_DIR = PROJECT_ROOT / "data" / "EC301" / "Patient1"
+    ELECTRODE_NAMES = ['LOFC7', 'LOFC8', 'LOFC9', 'LOFC10', 'LOFC1', 'LOFC2', 'LOFC3', 'LOFC4',
+                   'ROFC1', 'ROFC2', 'ROFC3', 'ROFC4', 'ROFC7', 'ROFC8', 'ROFC9', 'ROFC10',
+                   'LPI1', 'LPI2', 'LPI3', 'LPI4', 'LC1', 'LC2', 'LC3', 'LC4', 
+                   'RC1', 'RC2', 'RC3', 'RC4', 'RI1', 'RI2', 'RI3', 'RI4', 'RA1', 'RA2', 'RA3', 'RA4']
+
 
 
 # Raw input .mat files
@@ -45,22 +62,6 @@ EMOTION_MAP = {
     0: "No emotion", 1: "Amusement", 2: "Embarrassment", 3: "Anger", 4: "Confused",
     5: "Awe", 6: "Disgust", 7: "Fear", 8: "Affection", 9: "Sadness"
 }
-# # === Electrodes for the patient ===
-# ELECTRODE_NAMES = [
-#     'LOFC7', 'LOFC8', 'LOFC9', 'LOFC10', 'LOFC2', 'LOFC3', 'LOFC4', 'LOFC5',
-#     'ROFC2', 'ROFC3', 'ROFC4', 'ROFC5', 'ROFC7', 'ROFC8', 'ROFC9', 'ROFC10',
-#     'LAMY1', 'LAMY2', 'LAMY3', 'LAMY4',
-#     'LAINS1', 'LAINS2', 'LAINS3', 'LAINS4',
-#     'LCING2', 'LCING3', 'LCING4', 'LCING5',
-#     'RCING2', 'RCING3', 'RCING4', 'RCING5',
-#     'RAINS1', 'RAINS2', 'RAINS3', 'RAINS4',
-#     'RAMY1', 'RAMY2', 'RAMY3', 'RAMY4'
-# ]
-
-ELECTRODE_NAMES = ['LOFC7', 'LOFC8', 'LOFC9', 'LOFC10', 'LOFC1', 'LOFC2', 'LOFC3', 'LOFC4',
-                   'ROFC1', 'ROFC2', 'ROFC3', 'ROFC4', 'ROFC7', 'ROFC8', 'ROFC9', 'ROFC10',
-                   'LPI1', 'LPI2', 'LPI3', 'LPI4', 'LC1', 'LC2', 'LC3', 'LC4', 
-                   'RC1', 'RC2', 'RC3', 'RC4', 'RI1', 'RI2', 'RI3', 'RI4', 'RA1', 'RA2', 'RA3', 'RA4']
 
 N_ELECTRODES = len(ELECTRODE_NAMES)
 
